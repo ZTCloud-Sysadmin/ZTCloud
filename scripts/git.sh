b19f6e89863204ff8002dd339d99649947f5367f
Sysadmin <system@ztcloud.org>
2025-04-05 09:45:04 +0200

    fixed scripts dir

--- scripts/git.sh
+++ scripts/git.sh
@@ -5,7 +5,7 @@ set -e
 source /opt/ZTCloud/scripts/log.sh
 
 # --- Variables ---
-INSTALL_DIR="/opt/ZTCloud/installer"
+INSTALL_DIR="/opt/ZTCloud/"
 SCRIPTS_DIR="/opt/ZTCloud/scripts"
 REPO_URL="https://github.com/ZTCloud-Sysadmin/ZTCloud.git"
 REPO_BRANCH="main"
